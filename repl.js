99fb676aa5fa348338a22566c319bb941c2573eb
SechabaMadyibi <sechabamadyibi1@gmail.com>
2024-02-26 21:13:02 +0200

    update

--- repl.js
+++ repl.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose"),
   Course = require("./models/course");
 var testCourse, testSubscriber;
 mongoose.connect(
-  "mongodb+srv://sechabamadyibi1:12345@cluster0.zshclj7.mongodb.net/",
+  "mongodb://localhost:27017/confetti_cuisine",
   { useNewUrlParser: true }
 );
 mongoose.set("useCreateIndex", true);
